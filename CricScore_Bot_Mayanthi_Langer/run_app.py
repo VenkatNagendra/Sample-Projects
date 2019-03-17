491cd04bdbcf990509c23aabd2efb737de6032b1
VenkatNagendra <venkatnagendra46@gmail.com>
2019-03-17 22:27:06 +0530

    Update run_app.py
    Masked the values

--- CricScore_Bot_Mayanthi_Langer/run_app.py
+++ CricScore_Bot_Mayanthi_Langer/run_app.py
@@ -5,9 +5,9 @@ from rasa_slack_connector import SlackInput
 
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/cricscorenlu')
 agent = Agent.load('./models/dialouge', interpreter = nlu_interpreter)
-input_channel = SlackInput('xoxp-577750409252-577782185266-578870809207-1c1de3b609e8118ba21c5260c9c65a36',
-                            'xoxb-577750409252-577795462418-lDbvxAXFr0XElvHDBqUHW4Vd',
-                            'CDBHUzVkR4DK0J2bStNXZWvs',
+input_channel = SlackInput('OAuth Access Token',
+                            'Bot User OAuth Access Token',
+                            'Verification Token',
                              True)
 agent.handle_channel(HttpInputChannel(5004,'/',input_channel))
 
