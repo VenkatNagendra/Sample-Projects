c96d79cce320d92f65f2e2744ea075abfb4f8a9a
VenkatNagendra <venkatnagendra46@gmail.com>
2019-04-01 10:27:14 +0530

    Update cnn.py

--- Keras_CNN_Classification/Person_Classification/cnn.py
+++ Keras_CNN_Classification/Person_Classification/cnn.py
@@ -76,8 +76,4 @@ test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
-training_set.class_indices
-if result[0][0] == 1:
-    prediction = 'dog'
-else:
-    prediction = 'cat'
+print(training_set.class_indices)
